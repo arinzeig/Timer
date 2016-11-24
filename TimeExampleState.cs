424d1e5a039577cd4d39a1e04f3e08e912481d35
arinzeig <igwiloarinze@hotmail.com>
2016-11-24 14:42:26 +0000

    Add files via upload

---
+++ TimeExampleState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace Test
+{
+	public class TimeExampleState
+	{
+		public TimeExampleState()
+		{
+			
+		}
+
+		public int counter = 0;
+		public Timer tmr;
+	}
+}
---
+++ ViewController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+using System.Threading;
+using System.Text;
+using System.Drawing;
+using System.Linq;
+using UIKit;
+
+namespace Test
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+			TImerTestRun timer = new TImerTestRun();
+			timer.Run();
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+
+	class TImerTestRun
+	{
+		public TImerTestRun()
+		{
+
+		}
+
+		public void Run()
+		{
+			TimeExampleState s = new TimeExampleState();
+
+			// Create the delegate that invokes methods for the timer.
+			TimerCallback timerDelegate = new TimerCallback(CheckStatus);
+
+			// Create a timer that waits one second, then invokes every second.
+			Timer timer = new Timer(timerDelegate, s, 1000, 1000);
+
+			// Keep a handle to the timer, so it can be disposed.
+			s.tmr = timer;
+
+			// The main thread does nothing until the timer is disposed.
+			while (s.tmr != null)
+				Thread.Sleep(0);
+			Console.WriteLine("Timer example done.");
+		}
+		// The following method is called by the timer's delegate.
+
+		void CheckStatus(Object state)
+		{
+			TimeExampleState s = (TimeExampleState)state;
+			s.counter++;
+			Console.WriteLine("{0} Checking Status {1}.", DateTime.Now.TimeOfDay, s.counter);
+			if (s.counter == 5)
+			{
+				// Shorten the period. Wait 10 seconds to restart the timer.
+				(s.tmr).Change(10000, 100);
+				Console.WriteLine("changed...");
+			}
+			if (s.counter == 10)
+			{
+				Console.WriteLine("disposing of timer...");
+				s.tmr.Dispose();
+				s.tmr = null;
+			}
+		}
+	}
+}
